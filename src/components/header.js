27813f4fca478c5a5e640fa7e1a59e391b1923cc
RaduCl <raduclitan@gmail.com>
2016-01-22 16:27:27 +0200

    added start coin generator

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Component } from 'react'
 
 export default class Header extends Component {
+    startingCoins(){
+        return Math.floor(Math.random()*20 + 20)
+    }
+
     render() {
         return (
             <div className="header">
                 <div className="button player-name" >Player One</div>
-                <div className="button player-name" >Left Coins 20</div>
+                <div className="button player-name" >Coins {this.startingCoins()}</div>
                 <div className="button player-name" >Player Two</div>
             </div>
         )
